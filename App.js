8d3ce3857cfc218be21a73ff9ff47fc9388ec397
Felipe Chandía <felipe@MacBook-Pro-de-Felipe-2.local>
2024-12-15 17:14:04 -0300

    expo-router 01

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, BackHandler } from 'react-native';
+import {useAuth0, Auth0Provider} from 'react-native-auth0';
 
 export default function App() {
+  const LogoutButton = () => {
+    const {clearSession} = useAuth0();
+
+    const onPress = async () => {
+        try {
+            await clearSession();
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    return <Button onPress={onPress} title="Log out" />
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Auth0Provider domain={"dev-rk0fl88jinxofajt.us.auth0.com"} clientId={"FylS8wC7JHGPyfeCYaSeXhEp5q1HRzaI"}>
+      <View style={styles.container}>
+      <LogoutButton />
+      <Text>this is the One</Text>
       <StatusBar style="auto" />
     </View>
+  </Auth0Provider>
+  
   );
 }
 
